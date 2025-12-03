546e5d6e0fcf889bb9d1907fbd4281091a070c04
Alejo <a.mgrau@sophometrics.nl>
2025-12-03 01:43:44 -0300

    Added form group and validators

--- src/app/components/register-form/register-form.ts
+++ src/app/components/register-form/register-form.ts
@@ -1,62 +1,111 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatSelectModule} from '@angular/material/select';
-import {provideNativeDateAdapter,  MAT_DATE_LOCALE} from '@angular/material/core';
-import { merge } from 'rxjs';
-
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { provideNativeDateAdapter, MAT_DATE_LOCALE } from '@angular/material/core';
+import { merge, min } from 'rxjs';
 
 @Component({
   selector: 'app-register-form',
   imports: [
     MatFormFieldModule,
     MatInputModule,
-    FormsModule,
     ReactiveFormsModule,
     MatButtonModule,
     MatIconModule,
     MatCardModule,
     MatDatepickerModule,
-    MatSelectModule
+    MatSelectModule,
   ],
-  providers: [{provide: MAT_DATE_LOCALE, useValue: 'es-AR'}, provideNativeDateAdapter()],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'es-AR' }, provideNativeDateAdapter()],
   templateUrl: './register-form.html',
-  styleUrl: './register-form.scss',
+  styleUrls: ['./register-form.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RegisterForm {
-  readonly countrySelected = signal('');
-  readonly email = new FormControl('', [Validators.required, Validators.email]);
+  private formBuilder = inject(FormBuilder);
 
-  errorMessage = signal('');
+  readonly hide = signal(true);
+  readonly errorMessage = signal('');
 
-  constructor() {
-    merge(this.email.statusChanges, this.email.valueChanges)
-      .pipe(takeUntilDestroyed())
-      .subscribe(() => this.updateErrorMessage());
+  public form: FormGroup = this.formBuilder.group(
+    {
+      fullName: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      date: [null, Validators.required],
+      areaCode: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(4),
+          Validators.pattern('^[0-9]*$'),
+        ],
+      ],
+      phone: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(8),
+          Validators.pattern('^[0-9]*$'),
+        ],
+      ],
+      country: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+    },
+    { validators: this.passwordMatchValidator }
+  );
+  
+
+  passwordMatchValidator(group: AbstractControl): ValidationErrors | null {
+    const password = group.get('password')?.value;
+  const confirmPassword = group.get('confirmPassword')?.value;
+  
+  if (password !== confirmPassword) {
+    group.get('confirmPassword')?.setErrors({ passwordMismatch: true });
+    return { passwordMismatch: true };
+  }
+  
+  return null;
   }
 
   updateErrorMessage() {
-    if (this.email.hasError('required')) {
+    if (this.form.get('email')?.hasError('required')) {
       this.errorMessage.set('You must enter a value');
-    } else if (this.email.hasError('email')) {
+    } else if (this.form.get('email')?.hasError('email')) {
       this.errorMessage.set('Not a valid email');
     } else {
       this.errorMessage.set('');
     }
   }
 
-  hide = signal(true);
-  clickEvent(event: MouseEvent) {
+  toggleHidePassword(event: MouseEvent) {
     this.hide.set(!this.hide());
     event.stopPropagation();
   }
 
- 
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('Form Submitted', this.form.value);
+    } else {
+      console.log('Form is invalid');
+      this.form.markAllAsTouched();
+    }
+  }
 }
